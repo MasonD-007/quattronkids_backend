be276d84b6dc3d29f70efa5ffa4e32d21ebba4be
MasonD-007 <masondrake305@gmail.com>
2024-07-25 22:21:23 -0700

    chore: Update Firebase dependencies and configuration files

---
+++ src/firebase.jsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { }
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJwJwwz91TY0n1aGCPHPfjvYfQS6ziuLo",
+  authDomain: "interviewebsite.firebaseapp.com",
+  projectId: "interviewebsite",
+  storageBucket: "interviewebsite.appspot.com",
+  messagingSenderId: "458737296748",
+  appId: "1:458737296748:web:20245acb9d0b184d733f49"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
