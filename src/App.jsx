895cdf2b25e323d47c8390542c3d37f6201117f2
MasonD-007 <masondrake305@gmail.com>
2024-07-28 14:26:16 -0700

    chore: Update react-csv-reader to version 4.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import React, { useState } from 'react';
-import { Firestore } from 'firebase/firestore';
+import React, {useEffect, useState} from 'react';
+import { getDoc, collection } from 'firebase/firestore';
+import { db } from './firebase';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [randomData, setRandomData] = useState([]);
 
- const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const text = e.target.result;
-      const rows = text.split('\n');
-      const data = rows.map(row => row.split(','));
-      setData(data);
-    };
-    reader.readAsText(file);
-  };
+  useEffect(() => {
+    handleDataRetrieve();
+  }, []);
 
-  const handleUpload = () => {
-    console.log("Getting data from the file");
+  const handleDataRetrieve = async () => {
+    const querySnapshot = await getDoc(collection(db, "data"));
+    const allData = querySnapshot.docs.map(doc => doc.data());
+    
+    // Shuffle the array and get 10 random elements
+    const shuffledData = allData.sort(() => 0.5 - Math.random());
+    const selectedData = shuffledData.slice(0, 10);
 
-
-    console.log("Uploading data to firestore");
-    Firestore.
-      .then(() => {
-        console.log("Data uploaded successfully");
-      })
-      .catch((error) => {
-        console.error("Error uploading data:", error);
-      });
+    setRandomData(selectedData);
   };
 
   return (
@@ -35,11 +25,17 @@ function App() {
       <header style={{ fontWeight: "bold", fontSize: "large" }}>
         Hello, I will be retrieving data from firestore in firebase
       </header>
-      <input type="file" accept=".csv" onChange={handleFileChange} />
 
-      <button style={{ margin: "12px", fontSize: "20px" }} onClick={handleUpload}>
-        Click me to upload the data to firestore
+      <button style={{ margin: "12px", fontSize: "20px" }} >
+        Refresh The page to get new random data
       </button>
+      <div>
+        {randomData.map((data, index) => (
+          <div key={index}>
+            {JSON.stringify(data)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
