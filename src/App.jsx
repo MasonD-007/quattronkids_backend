4d89b79360e962bd543dae73022a00fd08b8736b
MasonD-007 <masondrake305@gmail.com>
2024-07-28 20:58:30 -0700

    chore: Update data retrieval logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
-import React, {useEffect, useState} from 'react';
-import { collection, getDocs, limit, query } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, limit, query, where } from 'firebase/firestore';
 import { db } from './firebase';
 
 function App() {
   const [randomData, setRandomData] = useState([]);
-  
+
   async function fetchData() {
-      const querySnapshot = await getDocs(query(collection(db, "data"), limit(1000)));
-      const allData = querySnapshot.docs.map(doc => doc.data());
-      // Shuffle the array
-      const shuffledData = allData.sort(() => 0.5 - Math.random());
+    
+    const querySnapshot = await getDocs(query(collection(db, "data"), where("random", ">=", Math.random() * 100) ,limit(10)));
+    /*const allData = querySnapshot.docs.map(doc => doc.data());
+    // Shuffle the array
+    const shuffledData = allData.sort(() => 0.5 - Math.random());
 
-      // Get the first 10 items from the shuffled array
-      const selectedData = shuffledData.slice(0, 10);
+    // Get the first 10 items from the shuffled array
+    const selectedData = shuffledData.slice(0, 10);
 
-      setRandomData(selectedData);
-    }
+    setRandomData(selectedData);*/
+    const randData = querySnapshot.docs.map(doc => doc.data());
+    console.log(randData);
+    setRandomData(randData);
+
+  }
 
   useEffect(() => {
-    fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    //fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  function handleLocalStorage() {
+    console.log(localStorage)
+    localStorage.clear();
+    console.log("Local Storage Cleared");
+  };
 
   return (
     <div>
       <header style={{ fontWeight: "bold", fontSize: "large" }}>
-        Hello, I will be retrieving data from firestore in firebase
+        Hello, I will be retrieving data from Firestore in Firebase
       </header>
 
       <button style={{ margin: "12px", fontSize: "20px" }} onClick={fetchData}>
-        Refresh The Page or Click Here To Get New Data From The Database
+        Refresh The page to get new random data
       </button>
 
-      <div>
+      
         {randomData.map((data, index) => (
           <div key={index} style={{ margin: "12px" }}>
             <div style={{ fontWeight: "bold" }}>Data {index + 1}</div>
@@ -44,7 +54,8 @@ function App() {
             <li>{data.Narrative}</li>
           </div>
         ))}
-      </div>
+      
+        <button onClick={handleLocalStorage}>Clear Local Storage</button>
     </div>
   );
 }
