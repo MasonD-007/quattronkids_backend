cf79f665d1c518d4d18b176d55768444a853350a
MasonD-007 <masondrake305@gmail.com>
2024-07-28 17:41:38 -0700

    chore: Refactor data retrieval and display logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React, {useEffect, useState} from 'react';
-import { getDoc, collection } from 'firebase/firestore';
+import { collection, getDocs, limit, query } from 'firebase/firestore';
 import { db } from './firebase';
 
 function App() {
   const [randomData, setRandomData] = useState([]);
+  
+  async function fetchData() {
+      const querySnapshot = await getDocs(query(collection(db, "data"), limit(1000)));
+      const allData = querySnapshot.docs.map(doc => doc.data());
+      // Shuffle the array
+      const shuffledData = allData.sort(() => 0.5 - Math.random());
+
+      // Get the first 10 items from the shuffled array
+      const selectedData = shuffledData.slice(0, 10);
+
+      setRandomData(selectedData);
+    }
 
   useEffect(() => {
-    handleDataRetrieve();
+    fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleDataRetrieve = async () => {
-    const querySnapshot = await getDoc(collection(db, "data"));
-    const allData = querySnapshot.docs.map(doc => doc.data());
-    
-    // Shuffle the array and get 10 random elements
-    const shuffledData = allData.sort(() => 0.5 - Math.random());
-    const selectedData = shuffledData.slice(0, 10);
-
-    setRandomData(selectedData);
-  };
 
   return (
     <div>
@@ -26,13 +29,19 @@ function App() {
         Hello, I will be retrieving data from firestore in firebase
       </header>
 
-      <button style={{ margin: "12px", fontSize: "20px" }} >
-        Refresh The page to get new random data
+      <button style={{ margin: "12px", fontSize: "20px" }} onClick={fetchData}>
+        Refresh The Page or Click Here To Get New Data From The Database
       </button>
+
       <div>
         {randomData.map((data, index) => (
-          <div key={index}>
-            {JSON.stringify(data)}
+          <div key={index} style={{ margin: "12px" }}>
+            <div style={{ fontWeight: "bold" }}>Data {index + 1}</div>
+            <li>{data.Address}</li>
+            <li>{data.Contact}</li>
+            <li>{data.Email}</li>
+            <li>{data.Name}</li>
+            <li>{data.Narrative}</li>
           </div>
         ))}
       </div>
